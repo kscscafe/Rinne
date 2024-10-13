7e0ec3819209d624b897add5c0162c30ec6a2282
kscscafe <sugizaki502@yahoo.co.jp>
2024-10-13 21:26:22 +0900

    Initial Commit

---
+++ Rinne/RinneApp.swift
@@ -0,0 +1,17 @@
+//
+//  RinneApp.swift
+//  Rinne
+//
+//  Created by 杉崎康隆 on 2024/10/13.
+//
+
+import SwiftUI
+
+@main
+struct RinneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RinneTests/RinneTests.swift
@@ -0,0 +1,17 @@
+//
+//  RinneTests.swift
+//  RinneTests
+//
+//  Created by 杉崎康隆 on 2024/10/13.
+//
+
+import Testing
+@testable import Rinne
+
+struct RinneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
