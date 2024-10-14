7fc0cf05feedf1e276d0f8ae950be5fc5ef51baf
kscscafe <sugizaki502@yahoo.co.jp>
2024-10-14 12:23:17 +0900

    first commit

---
+++ Rinne/RotatingTextView.swift
@@ -0,0 +1,18 @@
+//
+//  RotatingTextView.swift
+//  Rinne
+//
+//  Created by 杉崎康隆 on 2024/10/13.
+//
+
+import SwiftUI
+
+struct RotatingTextView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RotatingTextView()
+}
